29172897c47eb9cdfb657abf9dd7bce0e162c1bb
M-Hatami <32916645+M-Hatami@users.noreply.github.com>
2022-09-20 15:09:50 +1000

    fix1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 import Shop from "./Shop";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Shop />
